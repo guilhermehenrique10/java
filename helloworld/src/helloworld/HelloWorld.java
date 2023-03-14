c244d3931c09e1f14124920c9ae6a70af81e2f4a
Guilherme Henrique <guilhermehenriquelima02@gmail.com>
2023-03-14 12:00:34 -0300

    Projeto Hello Word

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int teste = 10;
+		
+		System.out.println("Hello Word!");
+		System.out.println("\nOlá Turma 62!");
+		System.out.println("Variável teste: " + teste);
+
+	}
+
+}
