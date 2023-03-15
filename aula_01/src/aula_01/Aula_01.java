c53e6452c632fc3c47249f1951ef379e8ea88688
Guilherme Henrique <guilhermehenriquelima02@gmail.com>
2023-03-15 11:58:32 -0300

    Aula_01

--- aula_01/src/aula_01/Aula_01.java
+++ aula_01/src/aula_01/Aula_01.java
@@ -1,25 +1,38 @@
 package aula_01;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Aula_01 {
 	
 	public static void main(String[] args) {
 		
+		DecimalFormat df = new DecimalFormat("###,###,##0.00");
 		Scanner leia = new Scanner (System.in);
 		
-		int numero1, numero2; 
+		double numero1, numero2;
 		String nome;
 		
+		System.out.println("Digite o seu nome: ");
+		nome = leia.nextLine();
+		
 		System.out.println("Digite o primeiro número: ");
-		numero1 = leia.nextInt();
+		numero1 = leia.nextDouble();
 		
 		System.out.println("Digite o segundo número: ");
-		numero2 = leia.nextInt();
+		numero2 = leia.nextDouble();
 		
-		System.out.println("Soma: " + (numero1 + numero2 ));
+		System.out.println("Soma: " + df.format((numero1 + numero2 )));
+		System.out.println("Subtração: " + df.format((numero1 - numero2 )));
+		System.out.println("Multiplicação: " + df.format((numero1 * numero2 )));
+		System.out.println("Divisão: " + df.format((numero1 / numero2 )));
+		System.out.println("Potência: " + df.format(Math.pow(numero1, numero2 )));
+		System.out.println("Raíz Quadrada: " + df.format(Math.sqrt(numero1 )));
 		
-
+		System.out.println("Nome: " + nome);
+		
+		leia.close();
+		
 	}
 
 }
