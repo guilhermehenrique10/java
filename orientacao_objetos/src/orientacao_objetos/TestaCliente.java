683ce6e742dfd009d8223ae3b14792d55da05b88
Guilherme Henrique <guilhermehenriquelima02@gmail.com>
2023-03-27 17:52:15 -0300

    Orientacao Objetos

---
+++ orientacao_objetos/src/orientacao_objetos/TestaCliente.java
@@ -0,0 +1,13 @@
+package orientacao_objetos;
+
+public class TestaCliente {
+
+	public static void main(String[] args) {
+		
+		Cliente cl = new Cliente ("Maria", 1, 1000.00f, 2, 4);
+		
+		cl.visualizar();
+		
+	}
+
+}
